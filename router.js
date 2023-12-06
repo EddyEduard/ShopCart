7bcf2cefd1f632b428fee3d7948672b999cf379f
Eduard-Nicolae <eduard_nicolae@yahoo.com>
2023-12-06 12:31:40 +0200

    Routes

--- router.js
+++ router.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const index_router = require("./routers/index.router");
+const index_router = require("./routes/index.router");
 router.use("/", index_router);
 
-const auth_router = require("./routers/auth.router");
+const auth_router = require("./routes/auth.router");
 router.use("/auth", auth_router);
 
-const account_router = require("./routers/account.router");
+const account_router = require("./routes/account.router");
 router.use("/account", account_router);
 
-const shop_router = require("./routers/shop.router");
+const shop_router = require("./routes/shop.router");
 router.use("/shop", shop_router);
 
 module.exports = router;
